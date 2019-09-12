5d8229f52e2fec2700bd828a7fd6e26e69909021
anandsinghs257 <anandsinghs257@gmail.com>
2019-09-12 10:52:38 +0530

    webhook issue

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,6 +4,25 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 provider :shopify,
   ShopifyApp.configuration.api_key,
   ShopifyApp.configuration.secret,
+  scope: ShopifyApp.configuration.scope,
+  setup: lambda { |env|
+    strategy = env['omniauth.strategy']
+
+    shopify_auth_params = strategy.session['shopify.omniauth_params']&.with_indifferent_access
+    shop = if shopify_auth_params.present?
+      "https://#{shopify_auth_params[:shop]}"
+    else
+      ''
+    end
+
+    strategy.options[:client_options][:site] = shop
+    strategy.options[:old_client_secret] = ShopifyApp.configuration.old_secret
+  }
+# frozen_string_literal: true
+
+provider :shopify,
+  ShopifyApp.configuration.api_key,
+  ShopifyApp.configuration.secret,
   :redirect_uri =>  "https://tech-space.herokuapp.com/auth/shopify/callback",
   :callback_url => "https://tech-space.herokuapp.com/auth/shopify/callback",
   scope: ShopifyApp.configuration.scope,
